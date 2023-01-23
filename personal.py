e6822c6a0aec7d48b43dd9e280dc7aea29bb2816
choi4624 <choi4624@gmail.com>
2023-01-23 23:11:00 +0900

    Update and rename personal2.py to personal.py
    information attach

---
+++ personal.py
@@ -0,0 +1,14 @@
+## personal info
+chrome_driver_path = "/chromedriver_win32"
+naver_url = 
+craw_url = 
+naver_id = 
+naver_pw = 
+
+'''personal.py code look like this, fill the variable 
+chrome_driver_path = "/chromedriver_win32" 
+naver_url = "naver.com"
+craw_url = "cafe.naver.com/joonggonara"
+naver_id = "YOUR_LOGIN_id"
+naver_pw = "YOUR_LOGIN_PW, PLAIN TEXT" < BE CAREFUL
+'''
--- personal2.py
+++
@@ -1,6 +0,0 @@
-## personal info
-chrome_driver_path = "/chromedriver_win32"
-naver_url = 
-craw_url = 
-naver_id = 
-naver_pw = 
